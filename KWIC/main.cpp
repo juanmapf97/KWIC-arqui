ce88b707a27191cd058ba4becc4b7c0d697b6b61
juanmapf97 <juanmapf97@gmail.com>
2019-11-05 11:28:06 -0600

    Initial Commit

---
+++ KWIC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KWIC
+//
+//  Created by Juanma on 5/11/19.
+//  Copyright © 2019 Juanma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
